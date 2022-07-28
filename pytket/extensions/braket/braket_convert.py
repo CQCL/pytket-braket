e5a728a7799e3f9d22a421cd4301b97ae8b93d19
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2022-07-28 15:37:12 +0100

    update main from develop (#2)
    * fix disable workflow
    
    * set version to RC
    
    * second try RC1
    
    * update version
    
    * version update and remote tests
    
    * fix lint in braket
    
    * fix black
    
    * fix lint in braket

--- pytket/extensions/braket/braket_convert.py
+++ pytket/extensions/braket/braket_convert.py
@@ -27,9 +27,9 @@ from typing import (
     TypeVar,
     TYPE_CHECKING,
 )
-from pytket.circuit import Circuit, OpType  # type: ignore
-from braket.circuits import Circuit as BK_Circuit  # type: ignore
 from numpy import pi
+from braket.circuits import Circuit as BK_Circuit  # type: ignore
+from pytket.circuit import Circuit, OpType  # type: ignore
 
 if TYPE_CHECKING:
     from pytket.circuit import Node  # type: ignore
--- tests/conftest.py
+++ tests/conftest.py
@@ -17,8 +17,8 @@ import os
 import pytest
 import boto3
 from braket.aws.aws_session import AwsSession  # type: ignore
-from pytket.extensions.braket import BraketBackend
 from _pytest.fixtures import SubRequest
+from pytket.extensions.braket import BraketBackend
 
 
 def get_authenticated_aws_session(region: Optional[str] = None) -> Optional[AwsSession]:
@@ -33,7 +33,7 @@ def get_authenticated_aws_session(region: Optional[str] = None) -> Optional[AwsS
         # Note: this session fixture should be used when creating backends for tests
         #       where PYTKET_RUN_REMOTE_TESTS is true
         region_name = region or os.getenv("PYTKET_REMOTE_BRAKET_REGION")
-        boto_session = boto3.Session(
+        boto_session = boto3.Session(  # type: ignore
             aws_access_key_id=os.getenv("PYTKET_REMOTE_BRAKET_ACCESS_KEY_ID"),
             aws_secret_access_key=os.getenv("PYTKET_REMOTE_BRAKET_ACCESS_KEY_SECRET"),
             region_name=region_name,
