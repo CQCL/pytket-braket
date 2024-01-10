ca06e9b6f4a46c7d4751d5e706aa38f75b93cb4a
Melf <cqc@melf.de>
2024-01-10 14:52:58 +0000

    update pytket version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.22",
+        "pytket ~= 1.23",
         "amazon-braket-sdk ~= 1.53",
         "amazon-braket-schemas ~= 1.19",
         "amazon-braket-default-simulator ~= 1.20",
