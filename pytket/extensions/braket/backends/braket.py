a4101eda9fdf2869f5b1a7a0725451073c45b5ed
yoshi-qc <95134786+yoshi-qc@users.noreply.github.com>
2025-06-02 16:01:29 +0900

    Fix lint

--- pytket/extensions/braket/backends/braket.py
+++ pytket/extensions/braket/backends/braket.py
@@ -127,7 +127,7 @@ _gate_types = {
     "phase_damping": None,
     "phase_flip": None,
     "phaseshift": OpType.U1,
-    "prx": None,
+    "prx": OpType.PhasedX,
     "rx": OpType.Rx,
     "ry": OpType.Ry,
     "rz": OpType.Rz,
@@ -153,7 +153,6 @@ _gate_types = {
     "gpi": OpType.GPI,
     "gpi2": OpType.GPI2,
     "ms": OpType.AAMS,
-    "prx": OpType.PhasedX,
 }
 
 _multiq_gate_types = {
@@ -360,9 +359,8 @@ class BraketBackend(Backend):
                 self._device_type = _DeviceType.QPU
             else:
                 raise ValueError(f"Unsupported device type {aws_device_type}")
-        if self._verbatim:
-            if not aws_device_type == _DeviceType.QPU:
-                raise ValueError(f"The verbatim is not supported for {aws_device_type}")
+        if self._verbatim and not aws_device_type == _DeviceType.QPU:
+            raise ValueError(f"The verbatim is not supported for {aws_device_type}")
         props = self._device.properties.dict()
         action = props["action"]
         device_info = action.get(DeviceActionType.JAQCD)
@@ -373,11 +371,13 @@ class BraketBackend(Backend):
             raise ValueError(f"Unsupported device {device}")
 
         if self._verbatim:
-            supported_ops = set(op.lower() for op in props["paradigm"]["nativeGateSet"])
-        else:
             supported_ops = set(
-                op.lower() for op in device_info["supportedOperations"]
+                op.lower() for op in props["paradigm"]["nativeGateSet"]
             )  # noqa: C401
+        else:
+            supported_ops = set(  # noqa: C401
+                op.lower() for op in device_info["supportedOperations"]
+            )
         supported_result_types = device_info["supportedResultTypes"]
         self._result_types = set()
         for rt in supported_result_types:
@@ -739,24 +739,22 @@ class BraketBackend(Backend):
     ) -> AwsQuantumTask | LocalQuantumTask:
         if self._device_type == _DeviceType.LOCAL:
             return self._device.run(bkcirc, shots=n_shots, **kwargs)
-        else:
-            if self._verbatim:
-                bkcirc_verbatim = braket.circuits.Circuit().add_verbatim_box(bkcirc)
-                return self._device.run(
-                    bkcirc_verbatim,
-                    self._s3_dest,
-                    shots=n_shots,
-                    disable_qubit_rewiring=True,
-                    **kwargs,
-                )
-            else:
-                return self._device.run(
-                    bkcirc,
-                    self._s3_dest,
-                    shots=n_shots,
-                    disable_qubit_rewiring=self._supports_client_qubit_mapping,
-                    **kwargs,
-                )
+        elif self._verbatim:
+            bkcirc_verbatim = braket.circuits.Circuit().add_verbatim_box(bkcirc)
+            return self._device.run(
+                bkcirc_verbatim,
+                self._s3_dest,
+                shots=n_shots,
+                disable_qubit_rewiring=True,
+                **kwargs,
+            )
+        return self._device.run(
+            bkcirc,
+            self._s3_dest,
+            shots=n_shots,
+            disable_qubit_rewiring=self._supports_client_qubit_mapping,
+            **kwargs,
+        )
 
     def _to_bkcirc(
         self, circuit: Circuit
@@ -1012,9 +1010,7 @@ class BraketBackend(Backend):
                 if circuit_status.status is StatusEnum.ERROR:
                     raise RuntimeError(circuit_status.message)  # noqa: B904
                 time.sleep(wait)
-            raise RuntimeError(
-                f"Timed out: no results after {timeout} seconds."
-            )  # noqa: B904
+            raise RuntimeError(f"Timed out: no results after {timeout} seconds.")
 
     def _get_expectation_value(
         self,
