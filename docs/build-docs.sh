1380d77dfdddeb3a21ed7aa84712c0f7f14d6b17
Will Simmons <will.simmons1465@gmail.com>
2025-06-18 10:26:03 +0100

    No longer need navbar fix

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -6,8 +6,6 @@ cp -R pytket-docs-theming/quantinuum-sphinx .
 cp pytket-docs-theming/conf.py .
 # Get the name of the project
 EXTENSION_NAME="$(basename "$(dirname `pwd`)")"
-# Correct github link in navbar
-sed -i '' 's#CQCL/tket#CQCL/'$EXTENSION_NAME'#' _static/nav-config.js
 # Build the docs. Ensure we have the correct project title.
 sphinx-build -W -b html -D html_title="$EXTENSION_NAME" . build || exit 1
 sphinx-build -W -v -b coverage . build/coverage || exit 1
