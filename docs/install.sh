b4e5727a7cc68f714ae1ed1b2d5388c647150b1f
Melf <cqc@melf.de>
2025-06-26 22:28:47 +0100

    clean up

--- docs/install.sh
+++
@@ -1,6 +0,0 @@
-# Copy over poetry dependencies from theming repository
-
-# Install the docs dependencies. Creates a .venv directory in docs
-poetry install
-
-# NOTE: Editable wheel should be installed separately.
